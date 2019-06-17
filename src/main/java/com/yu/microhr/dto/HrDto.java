a2f8fed00e6b7ecc5ce4b99671f9905e257af177
Administrator <1724852741@qq.com>
2019-06-18 00:15:24 +0800

    second commit

---
+++ src/main/java/com/yu/microhr/dto/HrDto.java
@@ -0,0 +1,50 @@
+package com.yu.microhr.dto;
+
+import com.yu.microhr.bean.Hr;
+import com.yu.microhr.bean.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @创建人：于海波
+ * @创建时间： 2019/6/18
+ * @描述：
+ */
+public class HrDto  extends Hr implements UserDetails {
+
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
